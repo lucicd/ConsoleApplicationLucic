e96536f29faf6ad2ef1d69938dbc41493d59a0fe
Drazen Lucic <lucicd@hotmail.com>
2019-01-08 21:01:47 +0400

    Add project files.

---
+++ ConsoleApplicationLucic/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApplicationLucic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Drazen Lucic";
+            string location = "Dubai, UAE";
+            Console.WriteLine($"My name is {fullName}.");
+            Console.WriteLine($"I am from {location}.");
+
+            var today = DateTime.Now;
+            Console.WriteLine($"Today is {today:d}.");
+
+            //The following code assure that the program will
+            //work for any year, not just for 2019.
+
+            //First, find current yeaar's Christmas
+            var nextChristmas = new DateTime(today.Year, 12, 25);
+            
+            //If the current date is greater than
+            //this year's Christmas, add one year (i.e. take next year's Christmas).
+            if (today > nextChristmas)
+            {
+                nextChristmas = nextChristmas.AddYears(1);
+            }
+
+            Console.WriteLine($"Next Christmas is on {nextChristmas:d}.");
+
+            var daysUntilChristmas = nextChristmas - today;
+            Console.WriteLine($"Days unitl next Christmas: {daysUntilChristmas.Days}.");
+
+            Console.Write("\nPress any key to end the program... ");
+            Console.ReadLine();
+        }
+    }
+}
